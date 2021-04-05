9ba63e8f40f0fa01cf3d9756ef6064682d2df4eb
Jose Lobato <joselobatof@gmail.com>
2021-04-04 19:45:34 -0500

    adding style to badge component

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
 import confLogo from '../images/badge-header.svg';
+import './styles/Badge.css';
 
 class Badge extends React.Component {
     render() {
         return (
-            <div>
-                 <div>
+            <div className="Badge">
+                 <div className="Badge__header">
                      <img src={confLogo} alt="logo de la conf" />
                 </div>
-                <div>
-                    <img src="https://www.gravatar.com/avatar?d=identicon" alt="avatar" />
-                    <h1>User <br /> Name</h1>
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="avatar" />
+                    <h1>Jomilto <br /> Fajardo</h1>
                 </div>
-                <div>
-                    <p>Frontend Engenier</p>
-                    <p>@user</p>
+                <div className="Badge__section-info">
+                    <h3>Frontend Engenier</h3>
+                    <div>@jomilto</div>
                 </div>
-                <div>
-                    <p>#platziConf</p>
+                <div className="Badge__footer">
+                    #platziConf
                 </div>
             </div>
         );
